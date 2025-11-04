79d78b1187e30da342b8e23401d9384f9518d9c4
JT <jstorbit@gmail.com>
2025-11-03 21:13:14 -0700

    Update convert function spelling error

--- assets/logic/index.js
+++ assets/logic/index.js
@@ -1,4 +1,4 @@
-const contertToCelsius = (temp) => {
+const convertToCelsius = (temp) => {
   const celsiusTemp = (temp - 32) / 1.8;
   const celsiusWholeTemp = Math.round(celsiusTemp);
   return celsiusWholeTemp;
