8c4007d5978bccf29ef0859142b28656a8be831b
JT <jstorbit@gmail.com>
2025-11-03 21:26:31 -0700

    Add cold statements in describe function

--- assets/logic/index.js
+++ assets/logic/index.js
@@ -5,17 +5,22 @@ const convertToCelsius = (temp) => {
 }
 
 const describeTemperature = (temp) => {
-  currentTemp = convertToCelsius(temp);
+  const currentTemp = convertToCelsius(temp);
+  const veryCold = "it is very cold";
+  const cold = "it is cold";
+  const warm = "it is warm";
+  const hot = "it is hot";
+  const veryHot = "it is very hot";
   // I understand if and else if statements from some previous experience
   // with Java, using switch statement for practice in addition to if, else if,
   // else statements.  I have commented out the if, else if and else statements
   // for this exercise below the switch statement.
   switch (currentTemp) {
     case value:
-      
+      console.log(`BRRRR it is ${currentTemp}°C, ${veryCold} outside!`)
       break;
     case value:
-      
+      console.log(`BRR is is ${currentTemp}°C, ${cold} outside!`)
       break;
     case value:
       
